2a70c2b3e3ec7ee7779c7186909bc588a9392285
KevMcEwan <kev_mcewan@hotmail.com>
2018-12-05 11:03:35 +0000

    Create Router MVP completed. GET and POST requests tested via Insomnia

--- server/server.js
+++ server/server.js
@@ -1,23 +1,23 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const body_parser = require('body-parser');
+const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
-// const create_router = require('./create_router.js');
+const createRouter = require('./helper/create_router.js');
 
 const publicPath = path.join(__dirname, '../client/public');
 app.use(express.static(publicPath));
 
+app.use(bodyParser.json());
+
 MongoClient.connect('mongodb://localhost:27017')
   .then( (client) => {
     const db = client.db('bucket_list');
-    const collection = db.collection("user");
+    const collection = db.collection('user');
     const router = createRouter(collection);
     app.use("/api/bucket_list", router);
   })
   .catch(console.err);
-
-app.use(body_parser.json());
 
 app.listen(3000, function () {
   console.log(`Listening on port ${ this.address().port }`);
